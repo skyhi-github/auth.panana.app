b376b462b4e9f2aa4d39ea505e0b7e43c85a58a5
skyhi-github <skyhi>
2025-08-18 23:27:28 +0700

    Updat(): verify flow

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
@@ -9,9 +9,26 @@ export class AppController {
   @Get()
   getHtml(@Res() res: Response): void {
     // Get HTML content from the service
+    console.log('Serving HTML content');
     const htmlContent = this.appService.getHtmlContent();
     
     // Send the HTML response
     res.send(htmlContent);
   }
+
+  @Post('auth/verify')
+  async login(@Body() Body, @Res() res: Response) {
+
+    console.log('Received login request:', Body.tgAuthResult);
+    res.cookie('auth_token', 'skyhi', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 3600000,
+    });
+
+    // Send response with a message
+    return res.status(200);
+  }
+
 }
